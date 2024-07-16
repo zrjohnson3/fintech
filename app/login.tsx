417934d6c7d80fcb3416553a32171d66add3cd8a
Zachary Johnson <zrjohnson3@gmail.com>
2024-07-16 19:24:39 -0400

    Completed login and signup components with styling and basic functionality.

--- app/login.tsx
+++ app/login.tsx
@@ -1,12 +1,128 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import { defaultStyles } from '@/constants/Styles';
+import React from 'react';
+import Colors from '@/constants/Colors';
+import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
-const Page = () => {
-    return (
-        <View>
-            <Text>Page</Text>
-        </View>
-    )
+enum SignInType {
+    Phone,
+    Email,
+    Google,
+    Apple
 }
 
-export default Page
+const Page = () => {
+    const [countryCode, setCountryCode] = React.useState('+1');
+    const [phoneNumber, setPhoneNumber] = React.useState('');
+    const keybordVerticalOffset = Platform.OS === 'ios' ? 80 : 0;
+
+    const onSignup = async (type: SignInType) => {
+        console.log('Login', countryCode, phoneNumber);
+        if (type === SignInType.Phone) {
+            console.log('Sign up with Phone');
+        } else if (type === SignInType.Email) {
+            console.log('Sign up with Email');
+        } else if (type === SignInType.Google) {
+            console.log('Sign up with Google');
+        } else if (type === SignInType.Apple) {
+            console.log('Sign up with Apple');
+        } else {
+            console.log('Sign up with other');
+        }
+    };
+
+    return (
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={keybordVerticalOffset}
+        >
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                <View style={defaultStyles.container}>
+                    <Text style={defaultStyles.header}>Welcome back!</Text>
+                    <Text style={defaultStyles.descriptionText}>Enter your phone number associated with your account.</Text>
+
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            placeholder='Country Code'
+                            style={[styles.input, { flex: 0.2, marginRight: 10 }]}
+                            value={countryCode}
+                            onChangeText={setCountryCode}
+                        />
+                        <TextInput
+                            placeholder='Phone number'
+                            style={[styles.input, { flex: 0.8 }]}
+                            keyboardType='numeric'
+                            value={phoneNumber}
+                            onChangeText={setPhoneNumber}
+                        />
+                    </View>
+
+                    <TouchableOpacity
+                        style={[
+                            defaultStyles.pillButton,
+                            phoneNumber !== '' ? styles.enabled : styles.disabled,
+                            { marginTop: 20 }
+                        ]}
+                        onPress={() => onSignup(SignInType.Phone)}
+                        disabled={phoneNumber === ''}
+                    >
+                        <Text style={defaultStyles.buttonText}>Signup</Text>
+                    </TouchableOpacity>
+
+                    <View style={{ flexDirection: "row", alignItems: 'center', gap: 16, paddingTop: 20 }}>
+                        <View style={{ flex: 1, height: StyleSheet.hairlineWidth, backgroundColor: Colors.gray }} />
+                        <Text style={{ color: Colors.gray, fontSize: 20 }}>OR</Text>
+                        <View style={{ flex: 1, height: StyleSheet.hairlineWidth, backgroundColor: Colors.gray }} />
+                    </View>
+
+                    <TouchableOpacity
+                        style={[defaultStyles.pillButton, { backgroundColor: 'white', flexDirection: 'row', gap: 16, marginTop: 20 }]}
+                        onPress={() => onSignup(SignInType.Email)}
+                    >
+                        <Ionicons name='mail' size={24} color='black' />
+                        <Text style={[defaultStyles.buttonText, { color: 'black' }]}>Sign up with Email</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[defaultStyles.pillButton, { backgroundColor: 'white', flexDirection: 'row', gap: 16, marginTop: 20 }]} onPress={() => onSignup(SignInType.Google)}>
+                        <Ionicons name='logo-google' size={24} color='black' />
+                        <Text style={[defaultStyles.buttonText, { color: 'black' }]}>Sign up with Google</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[defaultStyles.pillButton, { backgroundColor: 'white', flexDirection: 'row', gap: 16, marginTop: 20 }]} onPress={() => onSignup(SignInType.Apple)}>
+                        <Ionicons name='logo-apple' size={24} color='black' />
+                        <Text style={[defaultStyles.buttonText, { color: 'black' }]}>Sign up with Apple</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    );
+}
+
+export default Page;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginVertical: 20,
+        flexDirection: 'row',
+    },
+    input: {
+        borderColor: 'black',
+        borderRadius: 16,
+        padding: 15,
+        fontSize: 18,
+        backgroundColor: Colors.lightGray,
+    },
+    textLink: {
+        color: Colors.primary,
+        borderRadius: 10,
+        fontSize: 16,
+    },
+    enabled: {
+        backgroundColor: Colors.primary,
+    },
+    disabled: {
+        backgroundColor: Colors.primaryMuted,
+    },
+});
