2578ccab89a431e6f3ac10efcd8462ef1d04ac81
Zachary Johnson <zrjohnson3@gmail.com>
2024-07-17 14:41:53 -0400

    Added Clerk for Login and Sign up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,38 @@ import { useColorScheme } from '@/components/useColorScheme';
 import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { ClerkProvider } from '@clerk/clerk-expo';
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+import * as SecureStore from 'expo-secure-store';
+
+// Check for the Clerk Publishable Key
+console.log('Clerk Publishable Key:', CLERK_PUBLISHABLE_KEY);
+if (!CLERK_PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+}
+
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    }
+    catch (err: any) {
+      console.error('Failed to fetch the auth token', err);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    }
+    catch (err: any) {
+      console.error('Failed to save the auth token', err);
+      return;
+    }
+  }
+}
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -111,11 +143,12 @@ const InitialLayout = () => {
 const RootLayoutNav = () => {
 
   return (
-    <>
-      <StatusBar style='light' />
-      <InitialLayout />
-
-    </>
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <StatusBar style='light' />
+        <InitialLayout />
+      </GestureHandlerRootView>
+    </ClerkProvider>
   );
 }
 
