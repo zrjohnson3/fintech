244cfdf447bb3e5142de824028fc1be67c7e004c
Zachary Johnson <zrjohnson3@gmail.com>
2024-07-20 13:24:45 -0400

    Added "Add Money" functionality and transaction history to home screen

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -4,13 +4,36 @@ import Colors from '@/constants/Colors';
 import { defaultStyles } from '@/constants/Styles';
 import RoundBtn from '@/components/custom/RoundBtn';
 import Dropdown from '@/components/custom/Dropdown';
+import { useBalanceStore } from '@/store/balanceStore';
 
 const Page = () => {
 
-    const balance = 1420;
+    // const balance = 1420;
+
+    // Get the balance, runTransaction, transactions, clearTransactions from the balanceStore to use in this component
+    // This allows us to persiste the balance and transactions across the app
+    const { balance, runTransaction, transactions, clearTransactions } = useBalanceStore();
 
     const onAddMoney = () => {
         console.log('Add Money');
+
+        runTransaction({
+            id: Math.random(),
+            date: new Date().toDateString(),
+            title: 'Deposit',
+            type: 'deposit',
+            amount: 100,
+            description: 'Added $100 to account'
+        })
+    }
+
+    const viewTransactions = () => {
+        console.log('View Transactions');
+
+        transactions.forEach((transaction) => {
+            console.log(transaction.title, transaction.amount
+            );
+        })
     }
 
     return (
@@ -18,15 +41,50 @@ const Page = () => {
             <View style={styles.account}>
                 <View style={styles.row}>
                     <Text style={styles.balance}>Account Balance: </Text>
-                    <Text style={styles.currency}>${balance}</Text>
+                    <Text style={styles.currency}>${balance()}</Text>
                 </View>
             </View>
 
             <View style={styles.actionRow}>
                 <RoundBtn icon={'add'} text='Add Money' onPress={onAddMoney} />
-                <RoundBtn icon={'refresh'} text='Exchange' onPress={onAddMoney} />
-                <RoundBtn icon={'list'} text='Details' onPress={onAddMoney} />
+                <RoundBtn icon={'refresh'} text='Exchange' onPress={() => {
+                    console.log("Exchange");
+                    viewTransactions();
+                }} />
+                <RoundBtn icon={'list'} text='Details' onPress={() => console.log("Details")} />
                 <Dropdown />
+            </View>
+
+            <View style={styles.transactions}>
+                <Text style={styles.transactionHeader}>Transactions</Text>
+                {transactions.length === 0 ? (
+                    <Text style={{ padding: 10 }}>No transactions</Text>
+                ) : (
+                    <>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, paddingVertical: 10 }}>
+                            <View style={styles.transactionSubheaderView}>
+                                <Text style={styles.transactionSubheaderText}>Type</Text>
+                            </View>
+                            <View style={styles.transactionSubheaderView}>
+                                <Text style={styles.transactionSubheaderText}>Date</Text>
+                            </View>
+                            <View style={styles.transactionSubheaderView}>
+                                <Text style={styles.transactionSubheaderText}>Amount</Text>
+                            </View>
+                        </View>
+                        {transactions.map((transaction) => (
+                            <View key={transaction.id} style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10 }}>
+                                <Text style={styles.transactionText}>{transaction.title}</Text>
+                                <Text style={styles.transactionText}>{transaction.date}</Text>
+                                <Text style={styles.transactionText}>{transaction.type === 'deposit' ? '+ ' : '- '}${transaction.amount}</Text>
+                                {/* <Text style={styles.transactionText}>{transaction.amount}</Text> */}
+                            </View>
+                        ))}
+                        <Button title='Clear Transactions' onPress={clearTransactions} />
+                    </>
+
+                )}
+
             </View>
         </ScrollView>
     )
@@ -60,4 +118,35 @@ const styles = StyleSheet.create({
         margin: 10,
 
     },
+    transactions: {
+        margin: 10,
+        padding: 10,
+        backgroundColor: Colors.lightGray,
+        borderRadius: 10,
+    },
+    transactionHeader: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 5,
+    },
+    transactionText: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 5,
+    },
+    // transactionSubheader: {
+    //     fontWeight: 'bold',
+    //     fontSize: 14,
+    //     borderBottomColor: 'black',
+    //     borderBottomWidth: 1,
+    //     zIndex: 50,
+    // }
+    transactionSubheaderView: {
+        borderBottomWidth: 1,
+        borderBottomColor: 'black',
+    },
+    transactionSubheaderText: {
+        fontWeight: 'bold',
+        fontSize: 14,
+    }
 })
