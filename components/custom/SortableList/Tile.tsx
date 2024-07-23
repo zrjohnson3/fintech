8f528fb1db9d5f9c6dd43a1eba63077e8886e6b3
Zachary Johnson <zrjohnson3@gmail.com>
2024-07-22 20:17:04 -0400

    Created Drag and Drop Widgets using can-it-be-done-in-react-native "Chrome" library by wcandillion. Had to adjust files to work with my app.

---
+++ components/custom/SortableList/Tile.tsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import { SIZE } from './config';
+import Colors from '@/constants/Colors';
+import { useBalanceStore } from '@/store/balanceStore';
+import { Ionicons } from '@expo/vector-icons';
+
+const styles = StyleSheet.create({
+    container: {
+        width: SIZE - 20,
+        height: 150,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.25,
+        shadowRadius: 2,
+        elevation: 5,
+        padding: 14,
+        alignSelf: 'center',
+    },
+});
+interface TileProps {
+    id: string;
+    onLongPress: () => void;
+}
+
+const Tile = ({ id }: TileProps) => {
+    const { transactions } = useBalanceStore();
+
+    if (id === 'spent') {
+        return (
+            <View style={styles.container} pointerEvents="none">
+                <Text style={{ color: Colors.gray, fontWeight: '500', fontSize: 16, paddingBottom: 20 }}>
+                    Spent this month
+                </Text>
+                <Text style={{ color: Colors.dark.background, fontWeight: 'bold', fontSize: 26, paddingTop: 10 }}>
+                    $1025.25
+                </Text>
+            </View>
+        );
+    }
+
+    if (id === 'cashback') {
+        return (
+            <View
+                style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}
+                pointerEvents="none">
+                <View style={{ alignItems: 'center', justifyContent: 'center', gap: 10 }}>
+                    <View
+                        style={{
+                            height: 60,
+                            width: 60,
+                            borderRadius: 30,
+                            backgroundColor: Colors.primary,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <Text style={{ color: '#fff', fontWeight: 'bold', fontSize: 18 }}>5%</Text>
+                    </View>
+                    <Text style={{ color: Colors.gray, fontWeight: 'bold', fontSize: 18 }}>Cashback</Text>
+                </View>
+            </View>
+        );
+    }
+
+    if (id === 'recent') {
+        return (
+            <View style={styles.container} pointerEvents="none">
+                <View>
+                    <Text style={{ color: Colors.gray, fontWeight: '500', fontSize: 16 }}>
+                        Recent transaction
+                    </Text>
+
+                    {transactions.length === 0 && (
+                        <Text style={{ color: Colors.gray, fontWeight: 'bold', fontSize: 18, paddingTop: 10 }}>
+                            No transactions
+                        </Text>
+                    )}
+
+                    {transactions.length > 0 && (
+                        <>
+                            <Text
+                                style={{
+                                    color: Colors.dark.text,
+                                    fontWeight: 'bold',
+                                    fontSize: 18,
+                                    paddingVertical: 10,
+                                }}>
+                                {transactions[transactions.length - 1].amount}€
+                            </Text>
+                            <Text style={{ color: Colors.dark.background, fontWeight: 'bold', fontSize: 16 }}>
+                                {transactions[transactions.length - 1].description}
+                            </Text>
+                            {/* <Text style={{ color: Colors.dark2, fontWeight: 'bold', fontSize: 16 }}>
+                                ${transactions[transactions.length - 1].amount}
+                            </Text> */}
+
+                        </>
+                    )}
+                </View>
+            </View>
+        );
+    }
+
+    if (id === 'cards') {
+        return (
+            <View style={styles.container} pointerEvents="none">
+                <Text style={{ color: Colors.gray, fontWeight: '500', fontSize: 16 }}>Cards</Text>
+                <Ionicons
+                    name="card"
+                    size={50}
+                    color={Colors.primaryMuted}
+                    style={{ marginTop: 20, alignSelf: 'center' }}
+                />
+            </View>
+        );
+    }
+};
+
+export default Tile;
