d6f0a205b434e0999724b8c573a3b8c93adbcd0d
Zachary Johnson <zrjohnson3@gmail.com>
2024-10-27 21:30:22 -0400

    Started implementing the account modal

--- app/(authenticated)/(modals)/_layout.tsx
+++ app/(authenticated)/(modals)/_layout.tsx
@@ -1,22 +1,35 @@
-import { View, Text, Modal } from 'react-native'
+import { View, Text, Modal, TouchableOpacity } from 'react-native'
 import { Stack, useRouter, useSegments } from 'expo-router';
 import React from 'react'
 import { BlurView } from 'expo-blur'
 
-import { FontAwesome } from '@expo/vector-icons'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import CustomHeader from '@/components/custom/CustomHeader'
+import Colors from '@/constants/Colors';
 
 {/* Lock Screen - without any animation  */ }
 const ModalsLayout = () => {
+
+    const router = useRouter();
+
     return (
         <Stack
             screenOptions={{
-                headerShown: false,
-                animation: 'none',
+                animation: 'fade',
+                presentation: 'transparentModal',
+                headerStyle: { backgroundColor: 'transparent' },
+                headerTransparent: true,
+                title: '',
+                headerLeft: () => (
+                    <TouchableOpacity onPress={router.back}>
+                        <Ionicons name='close-outline' size={42} color={'#fff'} style={{ marginTop: 20 }} />
+                    </TouchableOpacity>
+                ),
+
             }}
         >
             <Stack.Screen name='lock' options={{ headerShown: false, animation: 'none' }} />
-        </Stack>
+        </Stack >
     )
 }
 
---
+++ app/(authenticated)/(modals)/account.tsx
@@ -0,0 +1,150 @@
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useAuth, useUser } from '@clerk/clerk-expo';
+import { defaultStyles } from '@/constants/Styles';
+import { BlurView } from 'expo-blur';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
+
+const Page = () => {
+    const { user } = useUser(); // Clerk's user hook
+    const { signOut } = useAuth();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [edit, setEdit] = useState(false);
+
+    useEffect(() => {
+        // Only update the state if the user object is available
+        if (user) {
+            setFirstName(user.firstName || '');
+            setLastName(user.lastName || '');
+        }
+    }, [user]); // Effect runs when the user data changes
+
+    const onSaveUser = async () => {
+        // Save user logic (if needed)
+        try {
+            await user?.update({
+                firstName: firstName!,
+                lastName: lastName!,
+            });
+            setEdit(false);
+        }
+        catch (error: any) {
+
+        }
+    };
+
+    const onCaptureImage = async () => {
+        // Capture image logic (if needed)
+    };
+
+    // Debugging user information
+    console.log('Account Page', user?.fullName, user?.firstName, user?.lastName);
+
+    return (
+        <BlurView intensity={80} tint='dark' style={{ flex: 1, paddingTop: 100, backgroundColor: 'rgba(0,0,0,0.3)' }}>
+            {/* <Text style={styles.header}>Account Page</Text> */}
+            {user && (
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={onCaptureImage} style={styles.captureButton}>
+                        {user.imageUrl && <Image source={{ uri: user.imageUrl }} style={styles.userImage} />}
+                    </TouchableOpacity>
+
+                    {/* <View style={{ flexDirection: 'row', gap: 6 }} */}
+                    {!edit && (
+                        <View style={styles.editRow}>
+                            <Text style={[styles.userName, { fontSize: 26, justifyContent: 'center' }]}>
+                                {firstName} {lastName}
+                            </Text>
+                            <TouchableOpacity style={{ flexDirection: 'row', gap: 6 }} onPress={() => setEdit(true)}>
+                                {/* <Text style={styles.editText}>Edit</Text> */}
+                                <Ionicons name='ellipsis-horizontal' size={24} color={Colors.primary} />
+                                {/* DROPDOWN MENU WHEN THE USER HITS THE ... (HAMBUGER MENU) */}
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                    {edit && (
+                        <View style={styles.editRow}>
+                            <TextInput
+                                style={[styles.userNameEditField, { fontSize: 26, justifyContent: 'center' }]}
+                                value={firstName}
+                                onChangeText={setFirstName}
+                            />
+                            <TextInput
+                                style={[styles.userNameEditField, { fontSize: 26, justifyContent: 'center' }]}
+                                value={lastName}
+                                onChangeText={setLastName}
+                            />
+                            <TouchableOpacity style={{ flexDirection: 'row', gap: 6 }} onPress={onSaveUser}>
+                                {/* DROPDOWN MENU WHEN THE USER HITS THE ... (HAMBUGER MENU) */}
+                                <Ionicons name='checkmark' size={24} color={Colors.primary} />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+            )
+            }
+        </BlurView >
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 30,
+        textAlign: 'center',
+        color: Colors.primary,
+        marginBottom: 20,
+    },
+    userName: {
+        fontSize: 25,
+        textAlign: 'center',
+        color: Colors.primary,
+        paddingRight: 10,
+        paddingLeft: 5,
+    },
+    userNameEditField: {
+        fontSize: 25,
+        textAlign: 'center',
+        color: Colors.primary,
+        paddingRight: 10,
+        paddingLeft: 5,
+        height: 44,
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: 'black',
+        backgroundColor: Colors.lightGray,
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 8,
+    },
+    editRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+        backgroundColor: Colors.background,
+        padding: 15,
+        borderRadius: 30,
+    },
+    captureButton: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        backgroundColor: Colors.gray,
+        alignContent: 'center',
+        justifyContent: 'center',
+    },
+    userImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+    },
+    editText: {
+        color: Colors.primary,
+        marginLeft: 10,
+    },
+});
+
+export default Page;
