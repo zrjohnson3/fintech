417117dd8894e4e90186026b13cb2e655cb999c6
Zachary Johnson <zrjohnson3@gmail.com>
2024-10-27 23:11:07 -0400

    Updated the user layout

--- app/(user)/_layout.tsx
+++ app/(user)/_layout.tsx
@@ -1,22 +1,34 @@
 import { View, Text, Modal } from 'react-native'
 import { Stack, useRouter, useSegments } from 'expo-router';
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 import { BlurView } from 'expo-blur'
 
-import { FontAwesome } from '@expo/vector-icons'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import CustomHeader from '@/components/custom/CustomHeader'
 
 {/* Lock Screen - without any animation  */ }
 const UserLayout = () => {
+
+    const router = useRouter();
+
     return (
         <Stack
             screenOptions={{
-                headerShown: false,
-                animation: 'none',
+                // header: CustomHeader,
+                // header: () => null,
+                headerLeft: () => (
+                    <TouchableOpacity onPress={router.back}>
+                        <Ionicons name='arrow-back' size={42} color={'black'} style={{ padding: 2 }} />
+                    </TouchableOpacity>
+                ),
+                animation: 'fade',
+                headerBackButtonMenuEnabled: true,
+
             }}
         >
-            <Stack.Screen name='forgotPassword' options={{ headerShown: false, animation: 'none' }} />
-            <Stack.Screen name='resetPassword' options={{ headerShown: false, animation: 'none' }} />
+            <Stack.Screen name='forgotPassword' options={{ headerShown: true, animation: 'none' }} />
+            <Stack.Screen name='resetPasscode' options={{ headerShown: false, animation: 'none' }} />
         </Stack>
     )
 }
